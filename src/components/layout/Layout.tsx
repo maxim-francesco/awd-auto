cccf2df09c6ded059330c58c7df4f61710ed3115
Francesco Maxim <maaximfrancesco@gmail.com>
2025-10-11 16:14:43 +0000

    Titlu: Implementarea unui Banner de Consimțământ pentru Cookies Conform

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,6 +6,7 @@ import Header from "./Header"
 import Footer from "./Footer"
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import awdLogo from "@/assets/awd-auto-logo.jpg"
+import CookieConsent from "react-cookie-consent"
 
 
 interface LayoutProps {
@@ -42,6 +43,22 @@ const Layout = ({ children }: LayoutProps) => {
         notification
         notificationSound
       />
+      <CookieConsent
+        location="bottom"
+        buttonText="Acceptă"
+        declineButtonText="Refuză"
+        cookieName="awdAutoCookieConsent"
+        style={{ background: "#2B373B", borderTop: "1px solid #4a5559" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px", background: "#fde047", borderRadius: "8px" }}
+        declineButtonStyle={{ margin: "10px 10px 10px 0", borderRadius: "8px" }}
+        expires={150}
+        enableDeclineButton
+      >
+        Acest site folosește cookie-uri pentru a îmbunătăți experiența utilizatorilor. Prin continuarea navigării, vă exprimați acordul cu folosirea acestora. Pentru mai multe detalii, vă rugăm să consultați{" "}
+        <a href="/politica-confidentialitate" style={{ color: "#fde047", textDecoration: "underline" }}>
+          Politica noastră de Confidențialitate
+        </a>.
+      </CookieConsent>
     </div>
   )
 }
